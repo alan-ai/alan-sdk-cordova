4b6ad28b8463917c222f10670d02fe4d96ca0e81
Sergey Yuryev <snyuryev@gmail.com>
2020-11-16 16:19:18 +0300

    Examples. Add Alan Button on device ready event.

--- examples/AlanBasicExample/www/js/index.js
+++ examples/AlanBasicExample/www/js/index.js
@@ -20,6 +20,12 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        cordova.exec(
+            function(data) { console.log(data);},
+            function(error) { console.log(error);},
+            "alanVoice",
+            "addButton",
+            ["1ee6a74d9cccea77faceb579449bc5142e956eca572e1d8b807a3e2338fdd0dc/stage"]);
     },
 
     // deviceready Event Handler
